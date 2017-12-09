64e0ddaf715e19adc71741445301561b48d8126c
Charles Moss <cmoss99@gmail.com>
2017-12-08 23:19:42 -0800

    jedi force push

---
+++ assets/javascript/questions.js
@@ -0,0 +1,188 @@
+var questionsLibrary = [
+	{
+	 	question: "What was Luke Skywalker mother's name?",
+	 	answers: ["Padme Amidala", "Leiola Votur", "Shera Jenn", "Teras Nobus"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/000.jpg"
+	},
+	{
+	 	question: "What was Luke Skywalker wife's name?",
+	 	answers: ["Jona Farrah Skywalker", "Mara Jade Skywalker", "Sara Skywalker", "Leona Skywalker"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/001.jpg"
+	},
+	{
+	 	question: "Which of the following is NOT the name of a Jedi?",
+	 	answers: ["Obi Wan Kenobi", "Luke Skywalker", "Han Solo", "Yoda"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/002.jpg"
+	},
+	{
+	 	question: "What weapon were Jedi Knights famed for using?",
+	 	answers: ["A Knife", "Brass Knuckles", "Light-Saber", "Bow-n-Arrow"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/003.jpg"
+	},
+	{
+	 	question: "Han Solo son is called?",
+	 	answers: ["Tony Solo", "Mark Solo", "Ben Solo", "Han Solo Jr."],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/004.jpg"
+	},
+	{
+	 	question: "What is Chewbaccas nickname?",
+	 	answers: ["Backbreaker", "Chewy", "Big Bear", "Gorilla Back"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/005.jpg"
+	},
+	{
+	 	question: "Who burned Darth Vader\'s face and body?",
+	 	answers: ["Obi Wan Kenobi", "Luke Skywalker", "Han Solo", "Yoda"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/006.jpg"
+	},
+	{
+	 	question: "What was the name of Darth Vader\'s spaceship?",
+	 	answers: ["Zone X", "Black Capsule", "Light Ship", "Death Star"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/007.jpg"
+	},
+	{
+	 	question: "Who sent Anakin to destroy the Jedi base?",
+	 	answers: ["Han Solo", "Obi Wan Kenobi", "Yoda", "Palpetine"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/008.jpg"
+	},
+	{
+	 	question: "What was the real identity between Luke and Leai?",
+	 	answers: ["Cousins", "Niece/Nephew", "Siblings", "Married"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/009.jpg"
+	},
+	{
+	 	question: "Where was Luke and Leai\'s mother?",
+	 	answers: ["Died in labor", "Died in battle", "Assasinated", "Died of cancer"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/010.jpg"
+	},
+	{
+	 	question: "What is the birth order of the siblings Luke and Leai?",
+	 	answers: ["1 year apart", "Leai is older by 3 years", "Luke is older by 3 years", "Twins"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/011.jpg"
+	},
+	{
+	 	question: "Which Jedi Masters taught Luke the force?",
+	 	answers: ["Princess Leai", "Darth Vader", "Obi Wan Kenobi & Yoda", "Han Solo & Chewbacca"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/012.jpg"
+	},
+	{
+	 	question: "Who broke Princess Leai out of captivity?",
+	 	answers: ["Batman", "Master Roshi", "Yoda", "Luke Skywalker & Han Solo"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/013.jpg"
+	},
+	{
+	 	question: "In the movie, what super-power is expressed?",
+	 	answers: ["The Force", "Light-Saber", "Teleportation", "Flying Ships"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/014.jpg"
+	},
+	{
+	 	question: "What is the name of Han Solo spaceship?",
+	 	answers: ["Arius", "The Ship", "Ghostrider", "The Millenium Falcon"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/015.jpg"
+	},
+	{
+	 	question: "Luke was Leai\'s...",
+	 	answers: ["Uncle", "Husband", "Cousin", "Brother"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/016.jpg"
+	},
+	{
+	 	question: "At the beginning of the story, who died first?",
+	 	answers: ["Darth Vader", "Yoda", "Luke\'s Mother", "None of these"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/017.jpg"
+	},
+	{
+	 	question: "Who was froze in a chamber from Darth Vader?",
+	 	answers: ["Yoda", "None of these", "Han Solo", "Chewbacca"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/018.jpg"
+	},
+	{
+	 	question: "How did Obi Wan Kenobi die?",
+	 	answers: ["Fell in Lava", "None of these", "Fighting Darth Vader", "Lost in space"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/019.jpg"
+	},
+	{
+	 	question: "Who cut off Luke Skywalker\'s hand?",
+	 	answers: ["Darth Vader", "Lord Sidious", "Darth Maul", "The Joker"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/020.jpg"
+	},
+	{
+	 	question: "Who killed Han Solo?",
+	 	answers: ["None of these", "Yoda", "Ben", "Chewbacca"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/021.jpg"
+	},
+	{
+	 	question: "What character is green?",
+	 	answers: ["Chewbacca", "Yoda", "Jabba", "Aquaman"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/022.jpg"
+	},
+	{
+	 	question: "How many children did Anakin have?",
+	 	answers: ["Eight", "Two", "None", "Three"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/023.jpg"
+	},
+	{
+	 	question: "Who Married Princess Leai?",
+	 	answers: ["Goku", "Chewbacca", "Darth Vader", "Han Solo"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/024.jpg"
+	},
+	{
+	 	question: "Return of the...?",
+	 	answers: ["Jedi", "Dark Side", "Wookies", "Zombies"],
+	 	correctAnswer: 0,
+	 	image: "assets/images/questionImages/025.jpg"
+	},
+	{
+	 	question: "Who was called Storm Troopers?",
+	 	answers: ["Police Officer\'s", "Enemy Soldiers", "Jedi Soldiers", "Yoda\'s friends"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/026.jpg"
+	},
+	{
+	 	question: "Which of the following wasn't on the Dark Side?",
+	 	answers: ["Darth Vader", "Darth Maul", "Chewbaccca", "Lord Sidious"],
+	 	correctAnswer: 2,
+	 	image: "assets/images/questionImages/027.jpg"
+	},
+	{
+	 	question: "Who was Darth Vader before he joined the Dark Dide?",
+	 	answers: ["A Storm Trooper", "Anakin Skywalker", "A Slave", "Homeless"],
+	 	correctAnswer: 1,
+	 	image: "assets/images/questionImages/028.jpg"
+	},
+	{
+	 	question: "Who made Star Wars?",
+	 	answers: ["James Cameron", "Quentin Tarentino", "Tyler Perry", "George Lucas"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/029.jpg"
+	},
+	{
+	 	question: "Lucas Films belongs to who?",
+	 	answers: ["Timothy", "Marvel", "David", "George"],
+	 	correctAnswer: 3,
+	 	image: "assets/images/questionImages/029.jpg"
+	}
+];
